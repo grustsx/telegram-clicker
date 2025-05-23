15604a2cf5fb8bc8be40f71a1025c0da44959bab
Gr.exe <snikita1999s@gmail.com>
2025-05-23 13:16:07 +0300

    change click api

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        telegram_id: tg?.WebApp?.initDataUnsafe?.user?.id,
+        telegram_user: tg?.WebApp?.initDataUnsafe?.user,
       }),
     });
   };
