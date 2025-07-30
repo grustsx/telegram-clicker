4665491a400776816608ad818b295361077e7315
Gr.exe <snikita1999s@gmail.com>
2025-07-30 17:28:51 +0300

    add sound fade

--- src/audio/musicController.ts
+++ src/audio/musicController.ts
@@ -1,70 +1,87 @@
 import { Howl } from 'howler';
 
+const FADE_TIME = 500;
+
 function createLoop(src: string) {
   return new Howl({
     src: [src],
     loop: true,
-    volume: 0.5,
-    mute: true,
+    volume: 0,
   });
 }
 
-const loops: Record<string, Howl> = {
-  mainBass: createLoop('/sounds/music/Tortik BABurial-esque XS.mp3'),
-  mainPad: createLoop('/sounds/music/Tortik Pad No.13.mp3'),
-  mainDrum: createLoop('/sounds/music/Tortik 10-Vintage Intelligent Drum.mp3'),
-  buildingDrum: createLoop(
-    '/sounds/music/Tortik 14-Amen VP9000 Remixed 06 175 BPM.mp3',
-  ),
-  buildingSynth: createLoop('/sounds/music/Tortik XIC 53 Hello Wendy.mp3'),
-  skillBass: createLoop('/sounds/music/Tortik BA 808 Bass XS.mp3'),
-  skillPad: createLoop('/sounds/music/Tortik PD Trance Voices.mp3'),
-  skillSnare: createLoop('/sounds/music/Tortik 4-Snare_7.mp3'),
-  skillKick: createLoop('/sounds/music/Tortik 5-Kick_25.mp3'),
-  skillHat: createLoop('/sounds/music/Tortik 6-Hihat_7_closed.mp3'),
-  bells: createLoop('/sounds/music/Tortik Rahels Long.mp3'),
+const loops: Record<string, { howl: Howl; id?: number | undefined }> = {
+  mainBass: { howl: createLoop('/sounds/music/Tortik BABurial-esque XS.mp3') },
+  mainPad: { howl: createLoop('/sounds/music/Tortik Pad No.13.mp3') },
+  mainDrum: {
+    howl: createLoop('/sounds/music/Tortik 10-Vintage Intelligent Drum.mp3'),
+  },
+  buildingDrum: {
+    howl: createLoop(
+      '/sounds/music/Tortik 14-Amen VP9000 Remixed 06 175 BPM.mp3',
+    ),
+  },
+  buildingSynth: {
+    howl: createLoop('/sounds/music/Tortik XIC 53 Hello Wendy.mp3'),
+  },
+  skillBass: { howl: createLoop('/sounds/music/Tortik BA 808 Bass XS.mp3') },
+  skillPad: { howl: createLoop('/sounds/music/Tortik PD Trance Voices.mp3') },
+  skillSnare: { howl: createLoop('/sounds/music/Tortik 4-Snare_7.mp3') },
+  skillKick: { howl: createLoop('/sounds/music/Tortik 5-Kick_25.mp3') },
+  skillHat: { howl: createLoop('/sounds/music/Tortik 6-Hihat_7_closed.mp3') },
+  bells: { howl: createLoop('/sounds/music/Tortik Rahels Long.mp3') },
 };
 
-export const startAllLoopsSync = () => {
-  const loadPromises: Promise<Howl>[] = [];
+const startAllLoopsSync = () => {
+  const loadPromises: Promise<{ howl: Howl; id?: number | undefined }>[] = [];
 
   for (const loop of Object.values(loops)) {
     loadPromises.push(
-      new Promise((resolve) => loop.once('load', () => resolve(loop))),
+      new Promise((resolve) => loop.howl.once('load', () => resolve(loop))),
     );
   }
   Promise.all(loadPromises).then((loops) => {
     loops.forEach((loop) => {
-      loop.play();
+      const id = loop.howl.play();
+      loop.id = id;
     });
   });
 };
 
 startAllLoopsSync();
 
+const playingLoopsSet = new Set<string>();
+
 export const unmuteSounds = (soundNames: string[]) => {
   soundNames.forEach((soundName) => {
     if (loops[soundName]) {
-      loops[soundName].mute(false);
+      playingLoopsSet.add(soundName);
+      loops[soundName].howl.fade(0, 0.5, FADE_TIME);
     }
   });
 };
 
 export const soloSounds = (soundNames: string[]) => {
-  for (const loop of Object.values(loops)) {
-    loop.mute(true);
-  }
+  Array.from(playingLoopsSet).forEach((playingSoundName) => {
+    if (soundNames.includes(playingSoundName)) return;
+
+    loops[playingSoundName].howl.fade(0.5, 0, FADE_TIME);
+    playingLoopsSet.delete(playingSoundName);
+  });
+
   soundNames.forEach((soundName) => {
-    if (loops[soundName]) {
-      loops[soundName].mute(false);
+    if (loops[soundName] && !playingLoopsSet.has(soundName)) {
+      playingLoopsSet.add(soundName);
+      loops[soundName].howl.fade(0, 0.5, FADE_TIME);
     }
   });
 };
 
 export const muteSounds = (soundNames: string[]) => {
   soundNames.forEach((soundName) => {
-    if (loops[soundName]) {
-      loops[soundName].mute(true);
+    if (loops[soundName] && playingLoopsSet.has(soundName)) {
+      playingLoopsSet.delete(soundName);
+      loops[soundName].howl.fade(0.5, 0, FADE_TIME);
     }
   });
 };
