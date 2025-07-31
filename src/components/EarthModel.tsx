04dbc16cf5ae21c7d4837545f656b63fda3006d2
Gr.exe <snikita1999s@gmail.com>
2025-07-31 09:25:29 +0300

    set Earth start position

--- src/components/EarthModel.tsx
+++ src/components/EarthModel.tsx
@@ -2,15 +2,23 @@ import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
+import React from 'react';
 
 export default function EarthModel() {
   const { scene } = useGLTF('/models/earth/scene.gltf');
   const groupRef = useRef<THREE.Group>(null);
 
+  React.useEffect(() => {
+    if (!groupRef.current) return;
+
+    const now = new Date();
+    groupRef.current.rotation.y =
+      (now.getHours() / 24) * 2 * Math.PI - Math.PI / 2;
+  }, []);
+
   useFrame(() => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += 0.0001;
-    }
+    if (!groupRef.current) return;
+    groupRef.current.rotation.y += 0.0001;
   });
 
   return (
