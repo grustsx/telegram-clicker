ab779d7222c86e162f14460ad377b448605e3c39
Gr.exe <snikita1999s@gmail.com>
2025-09-18 23:48:58 +0300

    debug

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect, type ReactElement } from 'react';
-import { selectAssetsLoading, selectLoading } from '../app/selectors';
+import {
+  selectAssetsLoading,
+  selectLoading,
+  selectUserId,
+} from '../app/selectors';
 import { useAppSelector } from '../app/hooks';
 import loadingGif from '../assets/loading.gif';
 
@@ -13,6 +17,7 @@ const Loader = ({ children }: { children: ReactElement }) => {
   const [messageIndex, setMessageIndex] = useState<number>(0);
   const loading = useAppSelector(selectLoading);
   const assetsLoading = useAppSelector(selectAssetsLoading);
+  const userId = useAppSelector(selectUserId);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -32,6 +37,7 @@ const Loader = ({ children }: { children: ReactElement }) => {
       <img src={loadingGif} alt="Загрузка..." className="w-64 h-64" />
       <div className="text-3xl text-tortik-white">
         {loadingMessagesArray[messageIndex]}
+        {userId}
       </div>
     </div>
   ) : (
