7ef420020172574da66bc859bfc75d6107ba21a8
Gr.exe <snikita1999s@gmail.com>
2025-10-25 15:16:50 +0300

    error message fix

--- src/components/ErrorHandler.tsx
+++ src/components/ErrorHandler.tsx
@@ -7,10 +7,10 @@ const ErrorHandler = ({ children }: { children: ReactElement }) => {
 
   return errorMessage ? (
     <div className="w-full h-full flex items-center justify-center bg-tortik-orange">
-      <div className="m-4 p-4 bg-tortik-white/5 text-tortik-white text-3xl border-dashed border-4 rounded-xl">
-        {'Ой, шото упало то ли у нас, то ли у вас, вот ошибка: '}
+      <div className="m-4 p-4 bg-tortik-white/5 text-tortik-white text-xl border-dashed border-4 rounded-xl">
+        {'Ошибка: '}
         <span className="text-tortik-yellow">{errorMessage}</span>
-        {'. Перезагрузите попробуйте игру хз'}
+        {'. Перезагрузите игру.'}
       </div>
     </div>
   ) : (
