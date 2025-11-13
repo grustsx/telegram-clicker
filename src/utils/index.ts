a2e66f0a113f0bed260cd043ceba81047656f075
Gr.exe <snikita1999s@gmail.com>
2025-11-13 18:53:28 +0300

    fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -37,7 +37,7 @@ export const getBuildingLevel = (
 const buildingSkillsMap: Record<number, number[]> = {
   1: [13, 26, 29],
   2: [14, 31, 33],
-  3: [15, 35, 21],
+  3: [15, 20, 21],
   4: [16, 51, 52],
   5: [17, 44, 46],
   6: [18, 47, 49],
