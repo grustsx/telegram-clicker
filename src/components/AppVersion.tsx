09a124d5b2e86dfdb4807fe9d99879e09849e1f1
Gr.exe <snikita1999s@gmail.com>
2025-11-13 12:08:46 +0300

    truncat

--- src/components/AppVersion.tsx
+++ src/components/AppVersion.tsx
@@ -1,12 +1,7 @@
-import { useAppSelector } from '../app/hooks';
-import { selectUserId } from '../app/selectors';
-
 export default function AppVersion() {
-  const id = useAppSelector(selectUserId);
-
   return (
     <div className="text-sm text-gray-400 absolute bottom-1 right-4">
-      {`${id} v${__APP_VERSION__}`}
+      {`v${__APP_VERSION__}`}
     </div>
   );
 }
