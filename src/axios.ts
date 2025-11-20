ad8611b7cce397c692fbe6c6845b602cf00308da
Gr.exe <snikita1999s@gmail.com>
2025-11-20 14:26:50 +0300

    add hack

--- src/axios.ts
+++ src/axios.ts
@@ -67,6 +67,7 @@ api.interceptors.response.use(
     }
 
     store.dispatch(setConnectionLoading(false));
+    if (message === 'Request failed with status code 404') return;
     store.dispatch(setErrorMessage(message));
     return Promise.reject(error); // пробрасываем ошибку дальше
   },
